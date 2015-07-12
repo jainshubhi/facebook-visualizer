60cdf1779993dfa40ff47119abe7d1b19b7a7f18
Shubhi <shubhijain@caltech.edu>
2015-07-11 21:05:53 -0700

    Started working on Facebook Api

--- apis/facebook_api.py
+++ apis/facebook_api.py
@@ -1,3 +1,12 @@
-import logging
-
 import requests
+
+
+class FacebookApi():
+
+    # facebook app configuration
+    APP_ID = ''
+    APP_SECRET = ''
+    CLIENT_CREDENTIALS = ''
+
+    def generate_token():
+        return ''
