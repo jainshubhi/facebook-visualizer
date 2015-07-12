10e1ef6ff2d5fab7d5f50714865fe6e7c0221f8b
Shubhi <shubhijain@caltech.edu>
2015-07-11 23:27:53 -0700

    Moved config to safer place

--- apis/facebook_api.py
+++ apis/facebook_api.py
@@ -3,10 +3,5 @@ import requests
 
 class FacebookApi():
 
-    # facebook app configuration
-    APP_ID = ''
-    APP_SECRET = ''
-    CLIENT_CREDENTIALS = ''
-
     def generate_token():
         return ''
